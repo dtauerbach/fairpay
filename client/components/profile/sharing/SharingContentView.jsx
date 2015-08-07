6217bc41675dd213bf3d40c59759884e0f55392b
Dan Auerbach <dtauerbach@gmail.com>
2015-08-06 19:07:14 -0700

    Implement basic radio button without two-way binding

--- client/components/profile/sharing/SharingContentView.jsx
+++ client/components/profile/sharing/SharingContentView.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {sharing_setting: 'private'};
+  },
+
+  handleChange: function(event) {
+    this.setState({sharing_setting: event.target.value})
+  },
+
   render: function() {
     return (
       <div id="profile-sharing-view">
-          This is the SHARING content view div
+          <input type="radio" className="radio sharing-settings-radio" name="sharing_setting_radio_selection" value="private" onChange={this.handleChange}> Private </input>
+          <input type="radio" className="radio sharing-settings-radio" name="sharing_setting_radio_selection" value="full" onChange={this.handleChange}> Full </input>
+          <div> Your sharing is set to {this.state.sharing_setting} </div>
       </div>
     );
   }
