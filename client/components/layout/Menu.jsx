d0e9ac3535587aad59b17a08d96655b1d6a80daf
Dan Auerbach <dtauerbach@gmail.com>
2015-08-10 15:12:58 -0700

    Get hello world for bootstrap menu working

--- client/components/layout/Menu.jsx
+++ client/components/layout/Menu.jsx
@@ -1,10 +1,33 @@
 var React = require('react');
 var Router = require('react-router');
+var ReactBootstrap = require('react-bootstrap');
 var Link = Router.Link;
+var Navbar = ReactBootstrap.Navbar;
+var Nav = ReactBootstrap.Nav;
+var NavItem = ReactBootstrap.NavItem;
+var DropdownButton = ReactBootstrap.DropdownButton;
+var MenuItem = ReactBootstrap.MenuItem;
+var Button = ReactBootstrap.Button;
 
 module.exports = React.createClass({
   render: function() {
-    return (
+    var navbarInstance = (
+      <Navbar brand='React-Bootstrap' className='fixedTop'>
+        <Nav>
+          <NavItem eventKey={1} href='#'>Link</NavItem>
+          <NavItem eventKey={2} href='#'>Link</NavItem>
+          <DropdownButton eventKey={3} title='Dropdown'>
+            <MenuItem eventKey='1'>Action</MenuItem>
+            <MenuItem eventKey='2'>Another action</MenuItem>
+            <MenuItem eventKey='3'><Button>Button</Button></MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey='4'>Separated link</MenuItem>
+          </DropdownButton>
+        </Nav>
+      </Navbar>
+   );
+
+    var oldmenu = (
       <div id="menu">
         <span id="menu-link" onClick={this.props.sendMenuClick}><span></span></span>
         <div id="menu-list">
@@ -19,5 +42,7 @@ module.exports = React.createClass({
         </div>
       </div>
     );
+
+    return navbarInstance;
   }
 });
--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,7 @@ require('./assets/blabs.css');
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./config/routes.jsx');
+var BootstrapWebpack = require('bootstrap-webpack');
 
 Router.run(routes, Router.HistoryLocation, function(Handler) {
   React.render(<Handler/>, document.body);
