f1c4804b22f24e5c08e1ff5cd79300f59640f331
Dan Auerbach <dtauerbach@gmail.com>
2015-08-10 21:25:24 -0700

    Switch left hand bar to top navbar via adding bootstrap

--- client/components/layout/Menu.jsx
+++ client/components/layout/Menu.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Router = require('react-router');
 var ReactBootstrap = require('react-bootstrap');
+var ReactRouterBootstrap = require('react-router-bootstrap');
 var Link = Router.Link;
 var Navbar = ReactBootstrap.Navbar;
 var Nav = ReactBootstrap.Nav;
@@ -8,41 +9,21 @@ var NavItem = ReactBootstrap.NavItem;
 var DropdownButton = ReactBootstrap.DropdownButton;
 var MenuItem = ReactBootstrap.MenuItem;
 var Button = ReactBootstrap.Button;
+var NavItemLink = ReactRouterBootstrap.NavItemLink;
 
 module.exports = React.createClass({
   render: function() {
     var navbarInstance = (
-      <Navbar brand='React-Bootstrap' className='fixedTop'>
-        <Nav>
-          <NavItem eventKey={1} href='#'>Link</NavItem>
-          <NavItem eventKey={2} href='#'>Link</NavItem>
-          <DropdownButton eventKey={3} title='Dropdown'>
-            <MenuItem eventKey='1'>Action</MenuItem>
-            <MenuItem eventKey='2'>Another action</MenuItem>
-            <MenuItem eventKey='3'><Button>Button</Button></MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey='4'>Separated link</MenuItem>
-          </DropdownButton>
+      <Navbar brand='FairPay' fixedTop>
+        <Nav bsStyle='pills' pullRight>
+          <NavItemLink to="about"> About </NavItemLink>
+          <NavItemLink to="profile"> My Profile </NavItemLink>
+          <NavItemLink to="data"> Market Data </NavItemLink>
+          <NavItem eventKey={1} href='#'>Sign Up</NavItem>
+          <NavItem eventKey={2} href='#'>Log In</NavItem>
         </Nav>
       </Navbar>
-   );
-
-    var oldmenu = (
-      <div id="menu">
-        <span id="menu-link" onClick={this.props.sendMenuClick}><span></span></span>
-        <div id="menu-list">
-          <div className="pure-menu pure-menu-open">
-            <span className="pure-menu-heading">Menu Heading</span>
-            <ul>
-              <li><Link to="profile">My Profile</Link></li>
-              <li><Link to="data">Compensation Data</Link></li>
-              <li><Link to="about">About</Link></li>
-            </ul>
-          </div>
-        </div>
-      </div>
     );
-
     return navbarInstance;
   }
 });
