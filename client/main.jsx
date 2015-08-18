ad4b3f4a17a2ef92f6ca116de0d48c5926e099eb
Dan Auerbach <dtauerbach@gmail.com>
2015-08-18 13:20:07 -0700

    Change order of CSS loads to override bootstrap CSS

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,8 @@
-require('./assets/fairpay.less');
-
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./config/routes.jsx');
 var BootstrapWebpack = require('bootstrap-webpack');
+var CustomCSS = require('./assets/fairpay.less');
 
 Router.run(routes, Router.HistoryLocation, function(Handler) {
   React.render(<Handler/>, document.body);
