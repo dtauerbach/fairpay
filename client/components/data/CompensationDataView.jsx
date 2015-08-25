69a01f4951669126162bbb10e856d8321a59329f
Dan Auerbach <dtauerbach@gmail.com>
2015-08-24 21:38:50 -0700

    Show two sad messages depending on whether user is signed in or not

--- client/components/data/CompensationDataView.jsx
+++ client/components/data/CompensationDataView.jsx
@@ -2,10 +2,18 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function() {
+    var notEnoughDataText = "Unfortunately, we don't yet have enough data to generate useful information for you :/ Please share with your friends.";
+    var connectFirst = "Please connect via LinkedIn in order to view market data.";
+    if (this.props.signedIn) {
+      var mainContentDiv = <div> {notEnoughDataText} </div>;
+    }
+    else {
+      var mainContentDiv = <div> {connectFirst} </div>
+    }
     return (
+
       <div id="compensation-data-view">
-        <h1>Some cool visualizations</h1>
-        <p>More goes here.</p>
+        {mainContentDiv}
       </div>
     );
   }
