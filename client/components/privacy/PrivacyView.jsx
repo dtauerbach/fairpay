eb682090d48669f5631d6b270ac8c07c65cbddd4
Dan Auerbach <dtauerbach@gmail.com>
2015-09-13 23:45:20 -0400

    Format tos and privacy policy pages

--- client/components/privacy/PrivacyView.jsx
+++ client/components/privacy/PrivacyView.jsx
@@ -5,71 +5,142 @@ module.exports = React.createClass({
     return (
       <div className="tos-view">
           <h1> Terms of Service </h1>
+          <p>
 1. Acceptance of the Terms of Service
+          </p>
 
-Welcome to FairPay! (a.k.a. "we" or "us" or the "Company"). We are excited to have you as user and member of the community. The following terms and conditions (collectively, these "Terms of Service") apply to your use of www.myfairpay.org, including any content, functionality and services offered on or via myfairpay.org (the "Website"). The Terms of Service also include our Privacy Policy, that you can review here
+          <p>
+Welcome to FairPay! (a.k.a. "we" or "us" or the "Company"). We are excited to have you as user and member of the community. The following terms and conditions (collectively, these "Terms of Service") apply to your use of www.myfairpay.org, including any content, functionality and services offered on or via myfairpay.org (the "Website"). The Terms of Service also include our Privacy Policy, that you can review here.
+          </p>
 
+          <p>
 We want to keep our relationship with you as lean and informal as possible, but please read the Terms of Service carefully before you start using FairPay, because by using the Website you accept and agree to be bound and abide by these Terms of Service.
+          </p>
 
+          <p>
 Should you disagree with some of the provisions herein, you can leave the Website (although we'll be sad to see you go!).
+          </p>
+
+          <p>
 2. Changes to the Terms of Service and the Website
+          </p>
 
+          <p>
 FairPay is a work in progress, meaning that a lot will change in the near future. We reserve the right to update the Website and these Terms of Service from time to time, at our discretion. We will make sure to announce any major change in a prominent way. Of course, this document is public on FairPay, and you will be able to see the changes for any new version. Your continued use of the Website following the publishing of updated Terms of Service means that you accept and agree to the changes.
+          </p>
+
+          <p>
 3. Accessing the Website,Security and Privacy
+          </p>
 
+          <p>
 We are working hard on improving FairPay, but we can't guarantee that the Website will be up and running 24/7. We also reserve the right to suspend or restrict access to some features to users. In any case, we will not be liable if for any reason all or any part of the Website is unavailable at any time or for any period, nor for any data loss (see also section 4 below).
+          </p>
 
+          <p>
 It is a condition of your use of the Website that all the information you provide on the Website is correct, current and complete. In the future, you may be asked to provide certain registration details or other information. As custom for internet websites, we reserve the right to disable any user account, at any time in our sole discretion for any or no reason, including, if in our opinion you have failed to comply with any provision of these Terms of Service.
+          </p>
 
+          <p>
 We use SSL encrypted browsing for all logged-in users, but we cannot guarantee that all use will be secure. We also do not guarantee that the Website or any content provided on the Website is error free. We manage your personal data according to our Privacy Policy.
+          </p>
+
+          <p>
 4. Disclaimer of Warranties, Limitations of Liability and Indemnification.
+          </p>
 
+          <p>
 Your use of FairPay is at your sole risk. The service is provided "as is" and "as available". We disclaim all warranties of any kind, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement. We are not liable for damages, direct or consequential, resulting from your use of the Website, and you agree to defend, indemnify and hold us harmless from any claims, losses, liability costs and expenses (including but not limited to attorney's fees) arising from your violation of any third-party's rights. You acknowledge that you have only a limited, non-exclusive, nontransferable license to use the Website. Because the Website is not error or bug free, you agree that you will use it carefully and avoid using it ways which might result in any loss of your or any third party's property or information.
+          </p>
+
+          <p>
 5. Governing Law and Jurisdiction
+          </p>
 
+          <p>
 These Terms of Service and any dispute or claim arising out of, or related to them, shall be governed by and construed in accordance with the internal laws of the State of New York without giving effect to any choice or conflict of law provision or rule.
+          </p>
 
+          <p>
 Any legal suit, action or proceeding arising out of, or related to, these Terms of Service or the Website shall be instituted exclusively in the federal courts of the United States or the courts of the State of New York.
+          </p>
+
+          <p>
 6. Waiver and Severability
+          </p>
 
+          <p>
 Our failure to exercise or enforce any right or provision of the Terms of Service shall not constitute a waiver of such right or provision. The Terms of Service constitutes the entire agreement between you and FairPay and govern your use of the service, superseding any prior agreements (including, but not limited to, any prior versions of the Terms of Service). If any provision of these Terms of Service is held by a court of competent jurisdiction to be invalid, illegal or unenforceable for any reason, such provision shall be eliminated or limited to the minimum extent such that the remaining provisions of the Terms of Service will continue in full force and effect.
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             <h1> Privacy </h1>
+          </p>
 
+<h1> Privacy </h1>
+
+<p>
 At FairPay, we collect and manage user data according to the following Privacy Policy, with the goal of incorporating our company values: transparency, accessibility, sanity, usability. This document is part of FairPay's Terms of Service, and by using myfairpay.org (the “Website”), you agree to the terms of this Privacy Policy and the Terms of Service. Please read the Terms of Service in their entirety, and refer to those for definitions and contacts.
+</p>
 
+<p>
 Data Collected
+</p>
 
+<p>
 We collect anonymous data from every visitor of the Website to monitor traffic and fix bugs. For example, we collect information like web requests, the data sent in response to such requests, the Internet Protocol address, the browser type, the browser language, and a timestamp for the request.
+</p>
 
+<p>
 We ask you to log in and provide certain compensation information related to your current employment. Unless we explicitly ask your permission, we will NOT store this compensation data alongside personally identifiable information such as your email address. We aim to keep your data as anonymous as possible, but do NOT guarantee that data that you share via the website will be anonymous. In particular, we cannot and do not guarantee the anonymity of sharing data, though attempting to de-anonymize the data is against the Terms of Service and spirit of the FairPay project.
+</p>
 
+<p>
 Minors and children should not use FairPay. By using the Website, you represent that you have the legal capacity to enter into a binding agreement.
+</p>
 
+<p>
 Use of the Data
+</p>
 
+<p>
 We only use your personal information to provide you the FairPay services or to communicate with you about the services or the Website.
+</p>
 
+<p>
 We employ industry standard techniques to protect against unauthorized access of data about you that we store, including personal information.
+</p>
 
+<p>
 Aside from the compensation data you are choosing to share, we do not share any personal information you have provided to us without your consent, unless:
+</p>
 
-    doing so is appropriate to carry out your own request;
-    we believe it's needed to enforce our Terms of Service, or that is legally required;
-    we believe it's needed to detect, prevent or address fraud, security or technical issues;
-    otherwise protect our property, legal rights, or that of others.
+<ul>
+    <li>doing so is appropriate to carry out your own request;</li>
+    <li>we believe it's needed to enforce our Terms of Service, or that is legally required;</li>
+    <li>we believe it's needed to detect, prevent or address fraud, security or technical issues;</li>
+    <li>otherwise protect our property, legal rights, or that of others.</li>
+    </ul>
 
+    <p>
 FairPay is operated from the United States. If you are visiting the Website from outside the U.S., you agree to any processing of any personal information you provide us according to this policy.
+    </p>
 
+    <p>
 Sharing of Data
+    </p>
 
+    <p>
 We don't share your personal information with third parties.We MAY use third party analytics services and other service providers in the future, though we do not do so now. All such service providers will be chosen so as to respect user data, from a security and privacy standpoint.
+    </p>
 
+    <p>
 The purpose of FairPay is to share data with other FairPay users, and so that is done as a matter of course. Please read sharing settings for information on how your data will be shared. You may change your sharing settings at any time.
+    </p>
 
+    <p>
 Changes to the Privacy Policy
+    </p>
 
+    <p>
 We may amend this Privacy Policy from time to time. Use of information we collect now is subject to the Privacy Policy in effect at the time such information is used. If we make major changes in the way we collect or use information, we will notify you by posting an announcement on the Website or sending you an email (if we have your email). A user is bound by any changes to the Privacy Policy when he or she uses the Services after such changes have been first posted.
-
+    </p>
 
       </div>
     );
