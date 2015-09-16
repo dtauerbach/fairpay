e40e71d6b5a1b547ba79db2ae028e2dae22bed4d
Dan Auerbach <dtauerbach@gmail.com>
2015-09-16 02:12:21 -0400

    Add correct links

--- client/components/about/AboutView.jsx
+++ client/components/about/AboutView.jsx
@@ -3,6 +3,7 @@ var ReactBootstrap = require('react-bootstrap');
 var Router = require('react-router');
 var Jumbotron = ReactBootstrap.Jumbotron;
 var Button = ReactBootstrap.Button;
+var Link = Router.Link;
 
 module.exports = React.createClass({
   contextTypes: {
@@ -14,9 +15,9 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    var fairPayAboutTextOne = 'FairPay is an experiment in compensation-sharing. By encouraging transparency and informed sharing, we hope to help ensure companies compensate employees fairly, regardless of gender, ethnicity, start date, or region of the world. Currently, we only support a few large tech companies, as we want to keep our questions as targeted as possible. The privacy philosophy of FairPay is to verify the data coming in as best we can, but not keep track of individuals beyond that verification step. Though we try our best to sanity check information, we cannot police every detail, so please be honest with the information you provide to this service.';
-    var fairPayAboutTextTwo = 'We believe strongly in user control, and as such we allow for different sharing options, and generally what you give is what you get: if you share a certain piece of information such as your level, then you will have access to others who have shared that piece of information. On the other hand, if you choose not to share a particular field, then you will not have access to data related to that field.';
-    var privacyPolicyAndFAQ = <div>Check out our <a href='/faq-and-privacy'>FAQ and Privacy Policy</a> for more information.</div>;
+    var fairPayAboutTextOne = 'FairPay is an experiment in compensation sharing. By encouraging transparency and informed sharing, we hope to help ensure companies compensate employees fairly, regardless of gender, ethnicity, start date, or region of the world. Currently, we only support a few large tech companies, as we want to keep our questions as targeted as possible. The privacy philosophy of FairPay is to verify the data coming in as best we can, but not keep track of individuals beyond that verification step. Though we try our best to sanity check information, we cannot police every detail or item of information that has been offered, so please be honest with the information you provide to this service.';
+    var fairPayAboutTextTwo = 'We believe strongly in user control, and as such we allow for different sharing options, and generally what you give is what you get: if you share a certain piece of information such as your level, then you will have access to the levels of other FairPay users who have shared that piece of information. On the other hand, if you choose not to share a particular field, then you will not have access to data related to that field.';
+      var privacyPolicyAndFAQ = <div>Check out our <Link to='/faq'>FAQ</Link> and <Link to='/privacy'>Terms of Service/Privacy Policy</Link> for more information.</div>;
     var introStyle = {
       background: 'black',
       backgroundSize: 'cover'
